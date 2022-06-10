9733e4962dadbc585ca51b2b3fc9842e4b2d0646
= <juan-manuel.macedo@soprasteria.com>
2022-06-10 18:32:22 +0200

    threeSum: still not fast enough

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,30 +1,33 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * This code has a O(n^3) Time complexity solution because of the 3 loops to arrive to the result.
+ */
 class ThreeSum {
-  public List<List<Integer>> threeSum(int[] nums) {
 
+  public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> triplets = new ArrayList<>();
-    for(int i = 0; i < nums.length; i++) {
-      for(int j = i+1; j < nums.length; j++) {
-        for(int k = j+1;  k < nums.length; k++) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
           int numero = nums[i] + nums[j] + nums[k];
-          boolean isRepeated = false;
-          if(numero == 0 && i != j && i != k && j != k) {
-            for(List<Integer> insideTriplets: triplets) {
-              List<Integer> newList = Arrays.asList(nums[i], nums[j], nums[k]);
-              System.out.println("InsideList: " + insideTriplets + " newList: " + newList);
-              isRepeated = insideTriplets.containsAll(newList) && newList.containsAll(insideTriplets);
-              if(isRepeated) break;
-            }
-            if(!isRepeated || triplets.isEmpty())
-              triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+          if (numero == 0 && i != j && i != k && j != k) {
+            List<Integer> orderList = Arrays.asList(nums[i], nums[j], nums[k]);
+            Collections.sort(orderList);
+            triplets.add(orderList);
           }
         }
       }
-
     }
+
+    Set<List<Integer>> set = new HashSet<>(triplets);
+    triplets.clear();
+    triplets.addAll(set);
     return triplets;
   }
 }

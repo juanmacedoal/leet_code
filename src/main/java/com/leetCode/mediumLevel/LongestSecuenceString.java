5afee30da07d2175268f2bfa5465835ee2523993
= <juan-manuel.macedo@soprasteria.com>
2022-07-20 14:00:10 +0200

    longestSecuenceString: improve time

--- src/main/java/com/leetCode/mediumLevel/LongestSecuenceString.java
+++ src/main/java/com/leetCode/mediumLevel/LongestSecuenceString.java
@@ -14,26 +14,57 @@ package com.leetCode.mediumLevel;
 public class LongestSecuenceString {
 
   public int lengthOfLongestSubstring(String s) {
+    long startTime = System.nanoTime();
+
     if(s.isEmpty()) return 0;
     String a = "";
     String c = "";
     int i = 0;
     StringBuilder stringBuilder = new StringBuilder();
-    for(char first: s.toCharArray()) {
-      for(int j = i; j < s.length(); j++) {
+    a: for(char first: s.toCharArray()) {
+      b: for(int j = i; j < s.length(); j++) {
         if(!stringBuilder.toString().contains(String.valueOf(s.charAt(j)))) {
           stringBuilder.append(s.charAt(j));
-        } else {
+        } else if(s.substring(i, s.length()).length() > stringBuilder.toString().length()) {
           if(stringBuilder.toString().length() > a.length()) a = stringBuilder.toString();
           stringBuilder.setLength(0);
           stringBuilder.append(s.charAt(j));
-        }
+        } else break a;
       }
       if(stringBuilder.toString().length() > c.length()) c = stringBuilder.toString();
       if(a.length() > c.length()) c = a;
       stringBuilder.setLength(0);
       i++;
     }
-    return a.length() > c.length() ? a.length() : c.length();
+    long endTime   = System.nanoTime();
+    long totalTime = endTime - startTime;
+    //System.out.println(totalTime);
+    return a.length() > stringBuilder.toString().length() ? a.length() : stringBuilder.toString().length();
   }
 }
+
+/**
+ *   public int lengthOfLongestSubstring(String s) {
+ *     if(s.isEmpty()) return 0;
+ *     String a = "";
+ *     String c = "";
+ *     int i = 0;
+ *     StringBuilder stringBuilder = new StringBuilder();
+ *     for(char first: s.toCharArray()) {
+ *       for(int j = i; j < s.length(); j++) {
+ *         if(!stringBuilder.toString().contains(String.valueOf(s.charAt(j)))) {
+ *           stringBuilder.append(s.charAt(j));
+ *         } else {
+ *           if(stringBuilder.toString().length() > a.length()) a = stringBuilder.toString();
+ *           stringBuilder.setLength(0);
+ *           stringBuilder.append(s.charAt(j));
+ *         }
+ *       }
+ *       if(stringBuilder.toString().length() > c.length()) c = stringBuilder.toString();
+ *       if(a.length() > c.length()) c = a;
+ *       stringBuilder.setLength(0);
+ *       i++;
+ *     }
+ *     return a.length() > c.length() ? a.length() : c.length();
+ *   }
+ */
